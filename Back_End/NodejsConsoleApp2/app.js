fc45d34b55ce55021ad47c41ad53376d2187c4c7
HyoSang <kimhys2@naver.com>
2016-05-13 11:41:06 +0900

    DB쪽 틀 구현

--- Back_End/NodejsConsoleApp2/app.js
+++ Back_End/NodejsConsoleApp2/app.js
@@ -5,6 +5,15 @@ var fs = require('fs')
 var bp = require('body-parser');
 var app = express();
 var mysql = require("mysql")
+var connection = mysql.createConnection({
+    host : 'localhost',
+    port : 3306,
+    user : 'root',
+    password : 'apmsetup',
+    database : 'SW'
+})
+connection.connect();
+
 app.use(bp.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
@@ -15,12 +24,45 @@ app.get('/', function (req, res) {
 app.post('/login', function (req, res) {
     var id = req.body.id;
     var pass = req.body.pass;
+    connection.query('Select ID from user where id = ?', [id], function (error, result)
+    {
+        if (result.ID == id) {
+            connection.query('Select Pass from user where id = ?', [id], function (error, result) {
+                if (result.Pass == pass) {
+                    req.session.id = id;
+                    //메인 화면으로 넘어감
+                }
+                else {
+                    //비밀번호가 다릅니다 신호
+                }
+            });
+        }
+        else {
+            //ID가 없습니다 신호
+        }
+    });
+
 
 });
 app.post('/signup', function (req, res) {
     var id = req.body.id;
     var pass = req.body.pass;
     var pN = req.body.pN;
+    connection.query('Select ID from user where id = ?', [id], function (error, result) {
+        if (result.id == id) {
+            //ID가 이미 존재합니다 신호
+       }
+        else {
+            connection.query('insert into user values(?,?,?)', [id,pass,pN], function (error, result) {
+                if (error) {
+
+                }
+                else {
+                    //회원가입 완료 신호
+                }
+            });
+        }
+    });
 });
 
 app.listen(3000, function () {
