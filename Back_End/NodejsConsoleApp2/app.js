14e6ea27a4f9bbf1eb252ab53435604214ac93c8
HyoSang <kimhys2@naver.com>
2016-05-17 09:38:30 +0900

    채팅  제작하는 중

--- Back_End/NodejsConsoleApp2/app.js
+++ Back_End/NodejsConsoleApp2/app.js
@@ -4,7 +4,9 @@ var swig = require('swig');
 var fs = require('fs')
 var bp = require('body-parser');
 var app = express();
-var mysql = require("mysql")
+var mysql = require("mysql");
+var io = require("socket.io").listen(httpServer);
+var http = require('http');
 var connection = mysql.createConnection({
     host : 'localhost',
     port : 3306,
@@ -12,13 +14,17 @@ var connection = mysql.createConnection({
     password : 'apmsetup',
     database : 'SW'
 })
+app.use(express.static(__dirname));
 connection.connect();
-
+var httpServer = http.createServer(app).listen(3000, function (req, res) {
+    console.log("Server on!");
+});
 app.use(bp.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    fs.readFile('Login.html', function (error, data) {
-        swig.renderFile('Login.html',function(error,data){res.send(data.toString())});
+    fs.readFile('Main.html', function (error, data) {
+        var data = swig.renderFile('Main.html', function (error, data){ res.send(data.toString()) });
+        res.send(data);
     });
 });
 app.post('/login', function (req, res) {
@@ -66,7 +72,34 @@ app.post('/signup', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log('server on!');
+io.sockets.on('connection', function (socket) {
+    // 사용자 채팅 메시지 broadcast
+    socket.on('user message', function (msg) {
+        socket.broadcast.emit('user message', socket.nickname, msg);
+    });
+    
+    // 사용자 접속시 Nickname 처리 public 
+    socket.on('nickname', function (nick, fn) {
+        if (nicknames[nick]) {
+            fn(true);
+        } else {
+            fn(false);
+            nicknames[nick] = socket.nickname = nick;
+            // 사용자 대화창에 알림 broadcast
+            socket.broadcast.emit('announcement', nick + '님 들어오셨습니다.');
+            // 전체 접속사용자 리스트 broadcast
+            socket.broadcast.emit('nicknames', nicknames);
+            socket.emit('nicknames', nicknames);
+        }
+    });
+    
+    socket.on('disconnect', function () {
+        if (!socket.nickname) return;
+        delete nicknames[socket.nickname];
+        socket.broadcast.emit('announcement', socket.nickname + '님 나가셨습니다.');
+        socket.broadcast.emit('nicknames', nicknames);
+    });
 });
+
+
 //소스트리 커밋테스트2
